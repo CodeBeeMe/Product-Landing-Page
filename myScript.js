23e63fdad63560e2c38239d9aa8f58dc175471d0
Cătălin Anghel-Ursu <catalee.xen@gmail.com>
2018-07-10 20:11:31 +0300

    Create myScript.js
    Added the .js file to the project's repository.

---
+++ myScript.js
@@ -0,0 +1,62 @@
+/* !Date:09.07.2018  Copyright ©2018 JavaScript code by Cătălin Anghel-Ursu @Madness2aMaze (https://codepen.io/Madness2aMaze)
+- All Rights Reserved!
+
+MIT License
+
+Copyright (c) 2018 Cătălin Anghel-Ursu (https://github.com/Madness2aMaze/Product-Landing-Page)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+$(document).ready(function() {
+  
+  let uiLogo = $("ui-logo"),
+      uiHome = $("#ui-home"),
+      uiDemo = $("#ui-demo"),      
+      uiFeatures = $("#ui-features"),      
+      uiSubscribe = $("#ui-subscribe");
+  
+  uiHome.click(() => {
+    uiHome.addClass("active");
+    uiDemo.removeClass("active");
+    uiFeatures.removeClass("active");
+    uiSubscribe.removeClass("active");
+  });
+  
+  uiDemo.click(() => {
+    uiDemo.addClass("active");
+    uiHome.removeClass("active");
+    uiFeatures.removeClass("active");
+    uiSubscribe.removeClass("active");
+  });
+  
+  uiFeatures.click(() => {
+    uiFeatures.addClass("active");
+    uiHome.removeClass("active");
+    uiDemo.removeClass("active");
+    uiSubscribe.removeClass("active");
+  });
+  
+  uiSubscribe.click(() => {
+    uiSubscribe.addClass("active");
+    uiHome.removeClass("active");
+    uiDemo.removeClass("active");
+    uiFeatures.removeClass("active");
+  });
+
+});
